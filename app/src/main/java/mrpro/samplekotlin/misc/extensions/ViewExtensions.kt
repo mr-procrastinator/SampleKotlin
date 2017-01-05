3081aed1fff5324ad62d6143d83b703e883f8050
Andrey T <a.a.tereschenko@gmail.com>
2017-01-05 13:52:15 +0300

    added image loading with using glide

--- app/src/main/java/mrpro/samplekotlin/misc/extensions/ViewExtensions.kt
+++ app/src/main/java/mrpro/samplekotlin/misc/extensions/ViewExtensions.kt
@@ -2,13 +2,25 @@
 
 package mrpro.samplekotlin.misc.extensions
 
+import android.content.Context
+import android.graphics.drawable.PictureDrawable
+import android.net.Uri
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import com.squareup.picasso.Picasso
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.model.StreamEncoder
+import com.bumptech.glide.load.resource.file.FileToStreamDecoder
+import com.caverock.androidsvg.SVG
 import mrpro.samplekotlin.R
+import mrpro.samplekotlin.misc.glide.SvgDecoder
+import mrpro.samplekotlin.misc.glide.SvgDrawableTranscoder
+import mrpro.samplekotlin.misc.glide.SvgSoftwareLayerSetter
+import java.io.InputStream
+
 
 /**
  * Created by Andrey on 11/29/16.
@@ -20,8 +32,36 @@ fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
 
 fun ImageView.loadImg(imageUrl: String) {
     if (TextUtils.isEmpty(imageUrl)) {
-        Picasso.with(context).load(R.mipmap.ic_launcher).into(this)
+        Glide.with(context)
+                .load(R.mipmap.ic_launcher).into(this)
+    } else if(imageUrl.endsWith("svg")){
+        val uri = Uri.parse(imageUrl)
+        getGlideSvgRequestBuilder(context)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                // SVG cannot be serialized so it's not worth to cache it
+                .load(uri)
+                .into(this)
     } else {
-        Picasso.with(context).load(imageUrl).into(this)
+        Glide.with(context)
+                .load(imageUrl)
+                .fitCenter()
+                .diskCacheStrategy( DiskCacheStrategy.SOURCE )
+                .placeholder(R.mipmap.ic_launcher)
+                .crossFade()
+                .into(this)
     }
+
 }
+
+fun getGlideSvgRequestBuilder(context : Context) = Glide.with(context)
+            .using(Glide.buildStreamModelLoader(Uri::class.java, context), InputStream::class.java)
+            .from(Uri::class.java)
+            .`as`(SVG::class.java)
+            .transcode(SvgDrawableTranscoder(), PictureDrawable::class.java)
+            .sourceEncoder(StreamEncoder())
+            .cacheDecoder(FileToStreamDecoder(SvgDecoder()))
+            .decoder(SvgDecoder())
+            .placeholder(R.mipmap.ic_launcher)
+            .error(R.mipmap.ic_launcher)
+            .animate(android.R.anim.fade_in)
+            .listener(SvgSoftwareLayerSetter<Uri>())
--- app/src/main/java/mrpro/samplekotlin/ui/adapters/delagates/TeamViewHolder.kt
+++ app/src/main/java/mrpro/samplekotlin/ui/adapters/delagates/TeamViewHolder.kt
@@ -2,13 +2,14 @@ package mrpro.samplekotlin.ui.adapters.delagates
 
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
-import mrpro.samplekotlin.ui.adapters.misc.AdapterViewItem
-import mrpro.samplekotlin.ui.adapters.misc.ViewType
 import kotlinx.android.synthetic.main.team_row_item.view.*
 import mrpro.samplekotlin.R
 import mrpro.samplekotlin.entities.Team
-import mrpro.samplekotlin.ui.adapters.misc.OnViewSelectedListener
 import mrpro.samplekotlin.misc.extensions.inflate
+import mrpro.samplekotlin.misc.extensions.loadImg
+import mrpro.samplekotlin.ui.adapters.misc.AdapterViewItem
+import mrpro.samplekotlin.ui.adapters.misc.OnViewSelectedListener
+import mrpro.samplekotlin.ui.adapters.misc.ViewType
 
 /**
  * Created by Andrey on 11/29/16.
@@ -20,7 +21,8 @@ class TeamViewHolder(parent: ViewGroup?, val listener: OnViewSelectedListener<Te
     }
 
     fun bind(item: Team) = with(itemView){
-            team_row_name.text = item.name;
+            team_row_name.text = item.name
+            team_row_icon.loadImg(item.crestUrl)
             setOnClickListener { listener.onItemSelected(item) }
     }
 }
